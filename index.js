75ad989bc204f6c6890a4ad9a3017c564e064ec4
GiaHuyCodeCode <hubby1364@gmail.com>
2024-08-24 18:28:09 +0700

    NhungBootstrap 4- Nhung file Tinh

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ require("dotenv").config();
 const route = require("./routes/client/index.route");
 
 const app = express();
+const port = process.env.PORT;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
-const port = process.env.PORT;
+
+app.use(express.static("public"));
 //Routes
 route(app);
 
